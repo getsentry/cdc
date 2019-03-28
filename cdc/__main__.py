a24230a5a9e62770f2c829279dd072201fbe274c
Ted Kaemming <ted@kaemming.com>
2019-03-28 11:43:06 -0700

    bring back `--log-level` to override root logger from cli

--- cdc/__main__.py
+++ cdc/__main__.py
@@ -7,12 +7,16 @@ from cdc.application import Application
 
 parser = optparse.OptionParser()
 parser.add_option('-f', '--configuration-file', default='configuration.yaml')
+parser.add_option('--log-level')
 options, arguments = parser.parse_args()
 
 configuration = yaml.load(open(options.configuration_file))
 
 if configuration['logging']:
     logging.config.dictConfig(configuration['logging'])
+
+if options.log_level is not None:
+    logging.getLogger().setLevel(logging._nameToLevel[options.log_level])
 
 application = Application(configuration)
 application.run()
