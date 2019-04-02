42cf0a807308f99e5dec5eeacf225997b8df5ade
Ted Kaemming <ted@kaemming.com>
2019-04-01 17:58:46 -0700

    don't poll source unless our last read was empty

--- cdc/application.py
+++ cdc/application.py
@@ -19,29 +19,40 @@ class Application(object):
         self.source.validate()
         self.publisher.validate()
 
+        iterations_without_source_message = 0
         try:
             message = None
             while True:
                 # In most circumstances, we won't have a message ready to be
                 # published and we'll need to fetch the next message to publish
-                # from the database. In some situations, there might already be
-                # a message from the last loop iteration that is still waiting
-                # to be sent. If that's the case, we don't need to get a new
+                # from the source. In some situations, there might already be a
+                # message from the last loop iteration that is still waiting to
+                # be sent. If that's the case, we don't need to get a new
                 # message.
                 if message is None:
                     logger.trace("Trying to fetch message from %r...", self.source)
                     message = self.source.fetch()
                     if message is not None:
-                        logger.trace("Received message: %r", message)
+                        logger.trace(
+                            "Received message (after %s attempts): %r",
+                            iterations_without_source_message,
+                            message,
+                        )
+                        iterations_without_source_message = 0
                     else:
-                        logger.trace("Did not receive message.")
+                        iterations_without_source_message += 1
+                        logger.trace(
+                            "Did not receive message (%s attempts so far.)",
+                            iterations_without_source_message,
+                        )
 
-                # It's also possible that there is nothing interesting
-                # happening on the database right now, so there are no messages
-                # to be sent. Normally, we'll have a message ready to go
-                # (either from the last loop iteration, or a new one that we
-                # fetched above) that we can try to publish. (If this fails,
-                # we'll try again on the next iteration.)
+                # It's also possible that we're all caught up, and there are no
+                # new changes available to be fetched from the source right
+                # now. In that case, there is no message to be sent. Normally,
+                # we'll have a message ready to go (either from the last loop
+                # iteration, or a new one that we fetched above) that we can
+                # try to publish. (If this fails, we'll try again on the next
+                # iteration.)
                 if message is not None:
                     logger.trace("Trying to write message to %r...", self.publisher)
                     try:
@@ -67,15 +78,14 @@ class Application(object):
                         self.source.set_write_position(message.id, message.position)
                         message = None
 
-                # If there are any queued delivery callbacks, we invoke them
-                # here.
+                # Invoke any queued delivery callbacks here, since these may
+                # change the deadline of any scheduled tasks.
                 logger.trace("Invoking queued delivery callbacks...")
                 self.publisher.poll(0)
 
-                # If there are any scheduled tasks that need to be performed
-                # (updating our slot positions, or sending keep-alive messages,
-                # for example), we run them now. Ideally, these tasks are non-
-                # or minimally blocking.
+                # If there are any scheduled tasks that need to be performed on
+                # the source (updating our positions, or sending keep-alive
+                # messages, for example), run them now.
                 now = datetime.now()
                 while True:
                     task = self.source.get_next_scheduled_task(now)
@@ -86,46 +96,42 @@ class Application(object):
                     logger.trace("Executing scheduled task: %r", task)
                     task.callable()
 
-                # We might need to block here if there are not any more
-                # messages ready to be fetched from the database, or if the
-                # publish queue is full and we are unable to add new messages
-                # to it. The easiest situation to identify is when the queue is
-                # full -- if this is the case, we'll still have a message ready
-                # to be published. In this case, we can poll the producer for
-                # updates. If there is no next message to be published, we're
-                # waiting on the database instead. Regardless of which event we
-                # are waiting on to occur, we need keep in mind the other tasks
-                # we need to accomplish in a reasonable timeframe: sending
-                # keep-alive messages, and updating our slot positions. We can
-                # only wait as long as the next deadline.
-                now = datetime.now()
-                task = self.source.get_next_scheduled_task(now)
-                max_loop_block_timeout = (
-                    10.0
-                )  # keep producer.poll from going totally unresponsive
-                if task is not None:
-                    timeout = min(
-                        max((task.deadline - now).total_seconds(), 0),
-                        max_loop_block_timeout,
-                    )
-                else:
-                    timeout = max_loop_block_timeout
+                # There are two situations where we need to block to avoid busy
+                # waiting. The first (and more obvious) one is if we still have
+                # a ``message`` record, since that signals that we failed to
+                # publish it during this current loop iteration. In that case,
+                # we need to wait for the publisher to have the capacity to
+                # accept a new message. The second is a little trickier -- to
+                # prevent a unnecessary (and potentially expensive) ``poll`` on
+                # the source, we only block on the source if the last two
+                # consecutive calls to ``source.fetch`` have both not returned
+                # a new message. If we need to block for either scenario, we
+                # also need to keep in mind that there are scheduled tasks that
+                # we have promised to execute within a reasonable timeframe. We
+                # can only wait as long as the next deadline.
+                if message is not None or iterations_without_source_message >= 2:
+                    now = datetime.now()
+                    task = self.source.get_next_scheduled_task(now)
+                    timeout = (task.deadline - now).total_seconds()
+                    if not timeout > 0:
+                        continue  # avoid blocking if we're past deadline
 
-                if message is None:
-                    logger.trace(
-                        "Waiting for %r for up to %0.4f seconds...",
-                        self.source,
-                        timeout,
-                    )
-                    self.source.poll(timeout)
-                else:
-                    logger.trace(
-                        "Waiting for %r for up to %0.4f seconds...",
-                        self.publisher,
-                        timeout,
-                    )
-                    self.publisher.poll(timeout)
-
+                    if message is None:
+                        logger.trace(
+                            "Waiting for %r for up to %0.4f seconds before next task: %r...",
+                            self.source,
+                            timeout,
+                            task,
+                        )
+                        self.source.poll(timeout)
+                    else:
+                        logger.trace(
+                            "Waiting for %r for up to %0.4f seconds next task: %r...",
+                            self.publisher,
+                            timeout,
+                            task,
+                        )
+                        self.publisher.poll(timeout)
         except KeyboardInterrupt as e:
             logger.debug("Caught %r, shutting down...", e)
             logger.debug(
