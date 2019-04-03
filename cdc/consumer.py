053d6e83d4dba4d9619d3efee6c6aa23d3c69d9f
Ted Kaemming <ted@kaemming.com>
2019-04-02 21:14:50 -0700

    break up work better in prep for multiproc

--- cdc/consumer.py
+++ cdc/consumer.py
@@ -5,13 +5,18 @@ import requests
 import tempfile
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Sequence
+from typing import Any, Iterable, Iterator, NamedTuple, Sequence
 from urllib.parse import urljoin, urlencode
 
 from cdc.logging import LoggerAdapter
 
 
 logger = LoggerAdapter(logging.getLogger(__name__))
+
+
+class Range(NamedTuple):
+    lo: Any
+    hi: Any
 
 
 @dataclass
@@ -40,8 +45,14 @@ class TableMapping(object):
 
 class SnapshotDataExporter(ABC):
     @abstractmethod
-    def dump(self, table: SourceTable, column_names: Sequence[str]):
+    def dump(self, table: SourceTable, column_names: Sequence[str], range: Range):
         pass
+
+
+@dataclass
+class ExportTask(object):
+    table: SourceTable
+    range: Range
 
 
 class SnapshotDataExporterManager(ABC):
@@ -52,13 +63,17 @@ class SnapshotDataExporterManager(ABC):
     def __exit__(self, exc_type, exc_value, traceback) -> bool:
         return False
 
+    @abstractmethod
+    def get_tasks(self, tables: Iterable[SourceTable]) -> Iterator[Range]:
+        pass
+
 
 class PostgresSnapshotDataExporter(SnapshotDataExporter):
     def __init__(self, dsn: str, exported_snapshot: str):
         self.__dsn = dsn
         self.__exported_snapshot = exported_snapshot
 
-    def dump(self, table: SourceTable, column_names: Sequence[str]):
+    def dump(self, table: SourceTable, column_names: Sequence[str], range: Range):
         with psycopg2.connect(self.__dsn) as connection:
             connection.autocommit = False
 
@@ -66,36 +81,20 @@ class PostgresSnapshotDataExporter(SnapshotDataExporter):
                 cursor.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE, READ ONLY")
                 cursor.execute("SET TRANSACTION SNAPSHOT %s", [self.__exported_snapshot])
 
-            chunk = 1000000
-
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT min({table.primary_key}), max({table.primary_key}) FROM {table.name}".format(table=table))
-                min_key, max_key = cursor.fetchone()
-                logger.trace("Fetched key range for %r: min=%s, max=%s", table, min_key, max_key)
-
-            for page in itertools.count(0):
-                with tempfile.NamedTemporaryFile(
-                    mode="wb",
-                    prefix="{}-{}-".format(table.name, page),
-                    delete=False,  # TODO: The loader should keep track and clean this up on exit.
-                ) as f, connection.cursor() as cursor:
-                    # TODO: Use ``psycopg2.sql`` instead here.
-                    key_lo = min_key + (chunk * page)
-                    key_hi = min_key + (chunk * (page + 1))
-                    logger.trace('Fetching page %s [%s:%s] from %r...', page, key_lo, key_hi, table)
-                    query = "COPY (SELECT {columns} FROM {table.name} WHERE {table.primary_key} >= {key_lo} AND {table.primary_key} < {key_hi} ORDER BY {table.primary_key} ASC) TO STDOUT".format(
-                        columns=", ".join(column_names),
-                        table=table,
-                        key_lo=key_lo,
-                        key_hi=key_hi,
-                        chunk=chunk,
-                    )
-                    cursor.copy_expert(query, f)
-                    logger.trace('Fetched page %s [%s:%s] from %r (%s rows.)', page, key_lo, key_hi, table, cursor.rowcount)
-                    yield open(f.name, "rb")
-
-                    if key_hi > max_key:
-                        break
+            with tempfile.NamedTemporaryFile(
+                mode="wb",
+                prefix="{table.name}-{range.lo}-{range.hi}".format(table=table, range=range),
+                delete=False,  # TODO: The loader should keep track and clean this up on exit.
+            ) as f, connection.cursor() as cursor:
+                logger.trace('Fetching %r from %r...', range, table)
+                query = "COPY (SELECT {columns} FROM {table.name} WHERE {table.primary_key} >= {range.lo} AND {table.primary_key} < {range.hi} ORDER BY {table.primary_key} ASC) TO STDOUT".format(
+                    columns=", ".join(column_names),
+                    table=table,
+                    range=range,
+                )
+                cursor.copy_expert(query, f)
+                logger.trace('Fetched %r from %r (%s rows.)', range, table, cursor.rowcount)
+                yield open(f.name, "rb")
 
 
 class PostgresSnapshotDataExporterManager(SnapshotDataExporterManager):
@@ -117,6 +116,24 @@ class PostgresSnapshotDataExporterManager(SnapshotDataExporterManager):
     def __exit__(self, exc_type, exc_value, traceback) -> bool:
         self.__connection.close()
         return False
+
+    def get_tasks(self, tables: Iterable[SourceTable]) -> Iterator[Iterator[Range]]:
+        for table in tables:
+            chunk = 1000000
+            with self.__connection.cursor() as cursor:
+                cursor.execute("SELECT min({table.primary_key}), max({table.primary_key}) FROM {table.name}".format(table=table))
+                min_key, max_key = cursor.fetchone()
+                logger.trace("Fetched key range for %r: min=%s, max=%s", table, min_key, max_key)
+
+            def get_ranges() -> Iterator[Range]:
+                for i in itertools.count(0):
+                    key_lo = min_key + (chunk * i)
+                    key_hi = min(min_key + (chunk * (i + 1)), max_key + 1)
+                    yield Range(key_lo, key_hi)
+                    if key_hi > max_key:
+                        break
+
+            yield get_ranges()
 
 
 class Loader(ABC):
@@ -146,13 +163,14 @@ class ClickhouseLoader(Loader):
 
 def bootstrap(export_manager: SnapshotDataExporterManager, loader: Loader, tables: Sequence[TableMapping]):
     with export_manager as snapshot_exporter:
-        for table in tables:
-            logger.debug("Loading data for %r...", table)
-            loader.load(
-                table.destination,
-                [column.destination_name for column in table.columns],
-                snapshot_exporter.dump(table.source, [column.source_name for column in table.columns])
-            )
+        for table, ranges in zip(tables, export_manager.get_tasks(table.source for table in tables)):
+            for range in ranges:
+                logger.debug("Loading data for %r (%r)...", table, range)
+                loader.load(
+                    table.destination,
+                    [column.destination_name for column in table.columns],
+                    snapshot_exporter.dump(table.source, [column.source_name for column in table.columns], range)
+                )
 
 
 if __name__ == '__main__':
@@ -167,6 +185,23 @@ if __name__ == '__main__':
                 ColumnMapping('aid', 'account_id'),
                 ColumnMapping('bid', 'branch_id'),
                 ColumnMapping('abalance', 'balance'),
+            ],
+        ),
+        TableMapping(
+            SourceTable('pgbench_branches', 'bid'),
+            DestinationTable('branches'),
+            [
+                ColumnMapping('bid', 'branch_id'),
+                ColumnMapping('bbalance', 'balance'),
+            ],
+        ),
+        TableMapping(
+            SourceTable('pgbench_tellers', 'tid'),
+            DestinationTable('tellers'),
+            [
+                ColumnMapping('tid', 'teller_id'),
+                ColumnMapping('bid', 'branch_id'),
+                ColumnMapping('tbalance', 'balance'),
             ],
         ),
     ])
