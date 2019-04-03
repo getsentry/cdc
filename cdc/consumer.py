453955eed4e9964a7405384d62f3b60666731e91
Ted Kaemming <ted@kaemming.com>
2019-04-02 21:15:35 -0700

    format

--- cdc/consumer.py
+++ cdc/consumer.py
@@ -78,22 +78,28 @@ class PostgresSnapshotDataExporter(SnapshotDataExporter):
             connection.autocommit = False
 
             with connection.cursor() as cursor:
-                cursor.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE, READ ONLY")
-                cursor.execute("SET TRANSACTION SNAPSHOT %s", [self.__exported_snapshot])
+                cursor.execute(
+                    "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE, READ ONLY"
+                )
+                cursor.execute(
+                    "SET TRANSACTION SNAPSHOT %s", [self.__exported_snapshot]
+                )
 
             with tempfile.NamedTemporaryFile(
                 mode="wb",
-                prefix="{table.name}-{range.lo}-{range.hi}".format(table=table, range=range),
+                prefix="{table.name}-{range.lo}-{range.hi}".format(
+                    table=table, range=range
+                ),
                 delete=False,  # TODO: The loader should keep track and clean this up on exit.
             ) as f, connection.cursor() as cursor:
-                logger.trace('Fetching %r from %r...', range, table)
+                logger.trace("Fetching %r from %r...", range, table)
                 query = "COPY (SELECT {columns} FROM {table.name} WHERE {table.primary_key} >= {range.lo} AND {table.primary_key} < {range.hi} ORDER BY {table.primary_key} ASC) TO STDOUT".format(
-                    columns=", ".join(column_names),
-                    table=table,
-                    range=range,
+                    columns=", ".join(column_names), table=table, range=range
                 )
                 cursor.copy_expert(query, f)
-                logger.trace('Fetched %r from %r (%s rows.)', range, table, cursor.rowcount)
+                logger.trace(
+                    "Fetched %r from %r (%s rows.)", range, table, cursor.rowcount
+                )
                 yield open(f.name, "rb")
 
 
@@ -107,7 +113,9 @@ class PostgresSnapshotDataExporterManager(SnapshotDataExporterManager):
         connection.autocommit = False
 
         with connection.cursor() as cursor:
-            cursor.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE, READ ONLY, DEFERRABLE")
+            cursor.execute(
+                "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE, READ ONLY, DEFERRABLE"
+            )
             cursor.execute("SELECT pg_export_snapshot()")
             self.__exported_snapshot = cursor.fetchone()[0]
 
@@ -121,9 +129,15 @@ class PostgresSnapshotDataExporterManager(SnapshotDataExporterManager):
         for table in tables:
             chunk = 1000000
             with self.__connection.cursor() as cursor:
-                cursor.execute("SELECT min({table.primary_key}), max({table.primary_key}) FROM {table.name}".format(table=table))
+                cursor.execute(
+                    "SELECT min({table.primary_key}), max({table.primary_key}) FROM {table.name}".format(
+                        table=table
+                    )
+                )
                 min_key, max_key = cursor.fetchone()
-                logger.trace("Fetched key range for %r: min=%s, max=%s", table, min_key, max_key)
+                logger.trace(
+                    "Fetched key range for %r: min=%s, max=%s", table, min_key, max_key
+                )
 
             def get_ranges() -> Iterator[Range]:
                 for i in itertools.count(0):
@@ -149,62 +163,83 @@ class ClickhouseLoader(Loader):
 
     def load(self, table: DestinationTable, column_names: Sequence[str], data) -> None:
         for page in data:
-            url = urljoin(self.__url, "?" + urlencode({
-                "database": self.__database,
-                "query": "INSERT INTO {table} ({columns}) FORMAT TabSeparated".format(
-                    table=table.name,
-                    columns=", ".join(column_names),
+            url = urljoin(
+                self.__url,
+                "?"
+                + urlencode(
+                    {
+                        "database": self.__database,
+                        "query": "INSERT INTO {table} ({columns}) FORMAT TabSeparated".format(
+                            table=table.name, columns=", ".join(column_names)
+                        ),
+                    }
                 ),
-            }))
+            )
             response = requests.post(url, data=page)
             response.raise_for_status()
             response.close()
 
 
-def bootstrap(export_manager: SnapshotDataExporterManager, loader: Loader, tables: Sequence[TableMapping]):
+def bootstrap(
+    export_manager: SnapshotDataExporterManager,
+    loader: Loader,
+    tables: Sequence[TableMapping],
+):
     with export_manager as snapshot_exporter:
-        for table, ranges in zip(tables, export_manager.get_tasks(table.source for table in tables)):
+        for table, ranges in zip(
+            tables, export_manager.get_tasks(table.source for table in tables)
+        ):
             for range in ranges:
                 logger.debug("Loading data for %r (%r)...", table, range)
                 loader.load(
                     table.destination,
                     [column.destination_name for column in table.columns],
-                    snapshot_exporter.dump(table.source, [column.source_name for column in table.columns], range)
+                    snapshot_exporter.dump(
+                        table.source,
+                        [column.source_name for column in table.columns],
+                        range,
+                    ),
                 )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=5)
-    exporter = PostgresSnapshotDataExporterManager('postgres://postgres@localhost:5432/postgres')
-    loader = ClickhouseLoader('http://localhost:8123', 'pgbench')
-    bootstrap(exporter, loader, [
-        TableMapping(
-            SourceTable('pgbench_accounts', 'aid'),
-            DestinationTable('accounts'),
-            [
-                ColumnMapping('aid', 'account_id'),
-                ColumnMapping('bid', 'branch_id'),
-                ColumnMapping('abalance', 'balance'),
-            ],
-        ),
-        TableMapping(
-            SourceTable('pgbench_branches', 'bid'),
-            DestinationTable('branches'),
-            [
-                ColumnMapping('bid', 'branch_id'),
-                ColumnMapping('bbalance', 'balance'),
-            ],
-        ),
-        TableMapping(
-            SourceTable('pgbench_tellers', 'tid'),
-            DestinationTable('tellers'),
-            [
-                ColumnMapping('tid', 'teller_id'),
-                ColumnMapping('bid', 'branch_id'),
-                ColumnMapping('tbalance', 'balance'),
-            ],
-        ),
-    ])
+    exporter = PostgresSnapshotDataExporterManager(
+        "postgres://postgres@localhost:5432/postgres"
+    )
+    loader = ClickhouseLoader("http://localhost:8123", "pgbench")
+    bootstrap(
+        exporter,
+        loader,
+        [
+            TableMapping(
+                SourceTable("pgbench_accounts", "aid"),
+                DestinationTable("accounts"),
+                [
+                    ColumnMapping("aid", "account_id"),
+                    ColumnMapping("bid", "branch_id"),
+                    ColumnMapping("abalance", "balance"),
+                ],
+            ),
+            TableMapping(
+                SourceTable("pgbench_branches", "bid"),
+                DestinationTable("branches"),
+                [
+                    ColumnMapping("bid", "branch_id"),
+                    ColumnMapping("bbalance", "balance"),
+                ],
+            ),
+            TableMapping(
+                SourceTable("pgbench_tellers", "tid"),
+                DestinationTable("tellers"),
+                [
+                    ColumnMapping("tid", "teller_id"),
+                    ColumnMapping("bid", "branch_id"),
+                    ColumnMapping("tbalance", "balance"),
+                ],
+            ),
+        ],
+    )
 
 
 class Consumer(ABC):
