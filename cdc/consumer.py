b858ff5362a119ffdf7350bde6ddd1e218d3bde5
Ted Kaemming <ted@kaemming.com>
2019-04-02 18:21:40 -0700

    chunk it up

--- cdc/consumer.py
+++ cdc/consumer.py
@@ -1,3 +1,4 @@
+import itertools
 import logging
 import psycopg2
 import requests
@@ -20,15 +21,26 @@ class ColumnMapping(object):
 
 
 @dataclass
+class SourceTable(object):
+    name: str
+    primary_key: str
+
+
+@dataclass
+class DestinationTable(object):
+    name: str
+
+
+@dataclass
 class TableMapping(object):
-    source_name: str
-    destination_name: str
+    source: SourceTable
+    destination: DestinationTable
     columns: Sequence[ColumnMapping]
 
 
 class SnapshotDataExporter(ABC):
     @abstractmethod
-    def dump(self, table_name: str, column_names: Sequence[str]):
+    def dump(self, table: SourceTable, column_names: Sequence[str]):
         pass
 
 
@@ -46,7 +58,7 @@ class PostgresSnapshotDataExporter(SnapshotDataExporter):
         self.__dsn = dsn
         self.__exported_snapshot = exported_snapshot
 
-    def dump(self, table_name: str, column_names: Sequence[str]):
+    def dump(self, table: SourceTable, column_names: Sequence[str]):
         with psycopg2.connect(self.__dsn) as connection:
             connection.autocommit = False
 
@@ -54,13 +66,36 @@ class PostgresSnapshotDataExporter(SnapshotDataExporter):
                 cursor.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE, READ ONLY")
                 cursor.execute("SET TRANSACTION SNAPSHOT %s", [self.__exported_snapshot])
 
-            with tempfile.NamedTemporaryFile(
+            chunk = 1000000
+
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT min({table.primary_key}), max({table.primary_key}) FROM {table.name}".format(table=table))
+                min_key, max_key = cursor.fetchone()
+                logger.trace("Fetched key range for %r: min=%s, max=%s", table, min_key, max_key)
+
+            for page in itertools.count(0):
+                with tempfile.NamedTemporaryFile(
                     mode="wb",
-                    prefix="{}-".format(table_name),
-                    delete=False,  # TODO: The loader might need to clean these up after it's done?
+                    prefix="{}-{}-".format(table.name, page),
+                    delete=False,  # TODO: The loader should keep track and clean this up on exit.
                 ) as f, connection.cursor() as cursor:
-                    cursor.copy_to(f, str(table_name), columns=column_names)
-                    return open(f.name, "rb")
+                    # TODO: Use ``psycopg2.sql`` instead here.
+                    key_lo = min_key + (chunk * page)
+                    key_hi = min_key + (chunk * (page + 1))
+                    logger.trace('Fetching page %s [%s:%s] from %r...', page, key_lo, key_hi, table)
+                    query = "COPY (SELECT {columns} FROM {table.name} WHERE {table.primary_key} >= {key_lo} AND {table.primary_key} < {key_hi} ORDER BY {table.primary_key} ASC) TO STDOUT".format(
+                        columns=", ".join(column_names),
+                        table=table,
+                        key_lo=key_lo,
+                        key_hi=key_hi,
+                        chunk=chunk,
+                    )
+                    cursor.copy_expert(query, f)
+                    logger.trace('Fetched page %s [%s:%s] from %r (%s rows.)', page, key_lo, key_hi, table, cursor.rowcount)
+                    yield open(f.name, "rb")
+
+                    if key_hi > max_key:
+                        break
 
 
 class PostgresSnapshotDataExporterManager(SnapshotDataExporterManager):
@@ -86,7 +121,7 @@ class PostgresSnapshotDataExporterManager(SnapshotDataExporterManager):
 
 class Loader(ABC):
     @abstractmethod
-    def load(self, table_name: str, column_names: Sequence[str], data) -> None:
+    def load(self, table: DestinationTable, column_names: Sequence[str], data) -> None:
         pass
 
 
@@ -95,17 +130,18 @@ class ClickhouseLoader(Loader):
         self.__url = url
         self.__database = database
 
-    def load(self, table_name: str, column_names: Sequence[str], data) -> None:
-        url = urljoin(self.__url, "?" + urlencode({
-            "database": self.__database,
-            "query": "INSERT INTO {table} ({columns}) FORMAT TabSeparated".format(
-                table=table_name,
-                columns=", ".join(column_names),
-            ),
-        }))
-        response = requests.post(url, data=data)
-        response.raise_for_status()
-        response.close()
+    def load(self, table: DestinationTable, column_names: Sequence[str], data) -> None:
+        for page in data:
+            url = urljoin(self.__url, "?" + urlencode({
+                "database": self.__database,
+                "query": "INSERT INTO {table} ({columns}) FORMAT TabSeparated".format(
+                    table=table.name,
+                    columns=", ".join(column_names),
+                ),
+            }))
+            response = requests.post(url, data=page)
+            response.raise_for_status()
+            response.close()
 
 
 def bootstrap(export_manager: SnapshotDataExporterManager, loader: Loader, tables: Sequence[TableMapping]):
@@ -113,22 +149,26 @@ def bootstrap(export_manager: SnapshotDataExporterManager, loader: Loader, table
         for table in tables:
             logger.debug("Loading data for %r...", table)
             loader.load(
-                table.destination_name,
+                table.destination,
                 [column.destination_name for column in table.columns],
-                snapshot_exporter.dump(table.source_name, [column.source_name for column in table.columns])
+                snapshot_exporter.dump(table.source, [column.source_name for column in table.columns])
             )
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=5)
     exporter = PostgresSnapshotDataExporterManager('postgres://postgres@localhost:5432/postgres')
     loader = ClickhouseLoader('http://localhost:8123', 'pgbench')
     bootstrap(exporter, loader, [
-        TableMapping('pgbench_accounts', 'accounts', [
-            ColumnMapping('aid', 'account_id'),
-            ColumnMapping('bid', 'branch_id'),
-            ColumnMapping('abalance', 'balance'),
-        ]),
+        TableMapping(
+            SourceTable('pgbench_accounts', 'aid'),
+            DestinationTable('accounts'),
+            [
+                ColumnMapping('aid', 'account_id'),
+                ColumnMapping('bid', 'branch_id'),
+                ColumnMapping('abalance', 'balance'),
+            ],
+        ),
     ])
 
 
