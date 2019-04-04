b7350657c17048621a7cadab3a56505b7020731b
Ted Kaemming <ted@kaemming.com>
2019-04-04 15:41:12 -0700

    back onto the write path

--- cdc/consumer.py
+++ cdc/consumer.py
@@ -4,6 +4,8 @@ import logging
 import psycopg2
 import requests
 import tempfile
+from clickhouse_driver import Client
+from collections import defaultdict
 from concurrent.futures import as_completed
 from concurrent.futures.process import ProcessPoolExecutor
 from confluent_kafka import Consumer as KakfaConsumer
@@ -11,7 +13,7 @@ from contextlib import contextmanager
 from dataclasses import dataclass
 from datetime import datetime
 from psycopg2 import sql
-from typing import Any, BinaryIO, Iterable, Iterator, Generator, NewType, Sequence, Tuple, Union
+from typing import Any, BinaryIO, Iterable, Iterator, Generator, MutableMapping, MutableSequence, NewType, Sequence, Tuple, Union
 from urllib.parse import urljoin, urlencode
 
 from cdc.logging import LoggerAdapter
@@ -20,12 +22,12 @@ from cdc.logging import LoggerAdapter
 logger = LoggerAdapter(logging.getLogger(__name__))
 
 
-@dataclass
+@dataclass(frozen=True)
 class Column:
     name: str
 
 
-@dataclass
+@dataclass(frozen=True)
 class Table:
     name: str
     identity_columns: Sequence[Column]
@@ -34,23 +36,26 @@ class Table:
     def __post_init__(self) -> None:
         assert len(self.identity_columns) > 0
 
+    def __hash__(self) -> int:
+        return hash(self.name)
+
     @property
     def columns(self) -> Iterator[Column]:
         return itertools.chain(self.identity_columns, self.data_columns)
 
 
-@dataclass
+@dataclass(frozen=True)
 class Source:
     table: Table
 
 
-@dataclass
+@dataclass(frozen=True)
 class Destination:
     table: Table
     version_column: Column
 
 
-@dataclass
+@dataclass(frozen=True)
 class TableMapping:  # not the best name
     source: Source
     destination: Destination
@@ -65,7 +70,7 @@ dump_dsn = 'postgres://postgres@localhost:5432/pgbench'
 SnapshotIdentifier = NewType('SnapshotIdentifier', str)
 
 
-@dataclass
+@dataclass(frozen=True)
 class Snapshot:
     xmin: int
     xmax: int
@@ -149,46 +154,57 @@ def bootstrap(table_mappings: Iterable[TableMapping]) -> Snapshot:
     return snapshot
 
 
-@dataclass
-class Begin:
+@dataclass(frozen=True)
+class Transaction:
     xid: int
     timestamp: datetime
 
 
-@dataclass
+@dataclass(frozen=True)
+class Begin:
+    transaction: Transaction
+
+
+@dataclass(frozen=True)
 class Insert:
-    table: str
+    table: Table
     new_identity_values: Sequence[Any]
     new_data_values: Sequence[Any]
 
 
-@dataclass
+@dataclass(frozen=True)
 class Update:
-    table: str
+    table: Table
     old_identity_values: Sequence[Any]
     new_identity_values: Sequence[Any]
     new_data_values: Sequence[Any]
 
 
-@dataclass
+@dataclass(frozen=True)
 class Delete:
-    table: str
+    table: Table
     old_identity_values: Sequence[Any]
 
 
-@dataclass
+@dataclass(frozen=True)
 class Commit:
     pass
 
 
-def decode(table_mappings: Iterable[TableMapping], message: str) -> Iterator[Union[Begin, Insert, Update, Delete, Commit]]:
+Mutation = Union[Insert, Update, Delete]
+Change = Union[Begin, Mutation, Commit]
+
+
+def decode(table_mappings: Iterable[TableMapping], message: str) -> Iterator[Change]:
     tables = {table_mapping.source.table.name: table_mapping for table_mapping in table_mappings}
 
     data = json.loads(message)
 
     yield Begin(
-        timestamp=datetime.strptime(data["timestamp"].split("+")[0], "%Y-%m-%d %H:%M:%S.%f"),  # XXX
-        xid=int(data['xid']),
+        Transaction(
+            timestamp=datetime.strptime(data["timestamp"].split("+")[0], "%Y-%m-%d %H:%M:%S.%f"),  # XXX
+            xid=int(data['xid']),
+        ),
     )
 
     for change in data["change"]:
@@ -200,22 +216,23 @@ def decode(table_mappings: Iterable[TableMapping], message: str) -> Iterator[Uni
             continue
 
         source_table = table_mapping.source.table
+        destination_table = table_mapping.destination.table
         if kind == "insert":
             yield Insert(
-                table=change["table"],
+                table=destination_table,
                 new_identity_values=[change["columnvalues"][i] for i in (change["columnnames"].index(column.name) for column in source_table.identity_columns)],
                 new_data_values=[change["columnvalues"][i] for i in (change["columnnames"].index(column.name) for column in source_table.data_columns)],
             )
         elif kind == "update":
             yield Update(
-                table=change["table"],
+                table=destination_table,
                 old_identity_values=[change["oldkeys"]["keyvalues"][i] for i in (change["oldkeys"]["keynames"].index(column.name) for column in source_table.identity_columns)],
                 new_identity_values=[change["columnvalues"][i] for i in (change["columnnames"].index(column.name) for column in source_table.identity_columns)],
                 new_data_values=[change["columnvalues"][i] for i in (change["columnnames"].index(column.name) for column in source_table.data_columns)],
             )
         elif kind == "delete":
             yield Delete(
-                table=change["table"],
+                table=destination_table,
                 old_identity_values=[change["oldkeys"]["keyvalues"][i] for i in (change["oldkeys"]["keynames"].index(column.name) for column in source_table.identity_columns)],
             )
         else:
@@ -234,7 +251,7 @@ stream_consumer_options = {
     'enable.partition.eof': 'false',
 }
 
-def stream(table_mappings: Iterable[TableMapping], snapshot: Snapshot) -> None:
+def consumer(table_mappings: Iterable[TableMapping]) -> Iterator[Change]:
     logger.debug('Starting to consume from stream...')
 
     consumer = KakfaConsumer(stream_consumer_options)
@@ -246,7 +263,56 @@ def stream(table_mappings: Iterable[TableMapping], snapshot: Snapshot) -> None:
             continue
 
         for change in decode(table_mappings, message.value()):
-            print(change)
+            yield change
+
+
+writer_host = "localhost"
+writer_database = "pgbench"
+
+def writer(changes: Iterator[Change], batch: int=1000) -> None:
+    client = Client(writer_host, database=writer_database)
+
+    inserts: MutableMapping[Table, MutableSequence[Sequence[Any]]] = defaultdict(list)
+    deletes: MutableMapping[Table, MutableSequence[Sequence[Any]]] = defaultdict(list)
+
+    pending = 0
+
+    for change in changes:
+        if not isinstance(change, (Insert, Update, Delete)):
+            logger.trace('Discarding non-mutation message: %r', change)
+            continue
+
+        if isinstance(change, Insert):
+            inserts[change.table].append([*change.new_identity_values, *change.new_data_values])
+        elif isinstance(change, Update):
+            inserts[change.table].append([*change.new_identity_values, *change.new_data_values])
+            if change.old_identity_values != change.new_identity_values:
+                deletes[change.table].append(change.old_identity_values)
+        elif isinstance(change, Delete):
+            deletes[change.table].append(change.old_identity_values)
+        else:
+            raise Exception
+
+        pending += 1
+
+        if pending > batch:
+            for table in list(inserts.keys()):
+                client.execute(
+                    "INSERT INTO {table.name} ({columns}) VALUES".format(
+                        table=table,
+                        columns=', '.join(column.name for column in table.columns),
+                    ),
+                    inserts.pop(table),
+                )
+
+            for table in list(deletes.keys()):
+                # TODO
+                deletes.pop(table)
+
+
+def stream(table_mappings: Iterable[TableMapping], snapshot: Snapshot) -> None:
+    # TODO: Snapshot filtering
+    writer(consumer(table_mappings))
 
 
 def setup_logging() -> None:
