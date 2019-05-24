befcaf804e573c8a050ee78c16baaf18d80bec0e
Matt Robenolt <matt@getsentry.com>
2019-05-24 11:05:13 -0700

    Make all the dockers nicer (#13)
    The primary changes are:
    
    * Introduce a docker-entrypoint script, which correctly drops permissions, and allows intercepting and say, hopping into bash.
    * Introduce gosu for dropping permissions, and tini as a zombie process reaper.
    * Only build the application target image, and not the full development one for production.
    * Introduce a `bin/cdc` wrapper to more simply differentiate between wanting say, a python REPL vs running `cdc` the application.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# first check if we're passing flags, if so
+# prepend with cdc
+if [ "${1:0:1}" = '-' ]; then
+    set -- cdc "$@"
+fi
+
+# Check for a valid cdc subcommand
+if cdc "$1" --help > /dev/null 2>&1; then
+    set -- cdc "$@"
+fi
+
+if [ "$1" = 'cdc' -a "$(id -u)" = '0' ]; then
+    set -- gosu cdc tini -- "$@"
+fi
+
+exec "$@"
