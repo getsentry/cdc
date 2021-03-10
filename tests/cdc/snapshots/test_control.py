0b87648bec71a1c1c29f0ec95784c3486dc6d805
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-03-09 18:51:07 -0800

    Improve testing

--- tests/cdc/snapshots/test_control.py
+++ tests/cdc/snapshots/test_control.py
@@ -1,3 +1,4 @@
+from cdc.streams.backends import ProducerBackend
 from cdc.streams.types import StreamMessage
 import json  # type: ignore
 from uuid import uuid1
@@ -10,9 +11,12 @@ from cdc.streams.producer import Producer as StreamProducer
 from cdc.snapshots.snapshot_control import SnapshotControl
 
 
-class DummyProducer(StreamProducer):
+class DummyProducerBackend(ProducerBackend):
     def __init__(self) -> None:
         self.items: List[bytes] = []
+
+    def __len__(self) -> int:
+        return len(self.items)
 
     def write(self, payload: StreamMessage, callback: Callable[[], None]) -> None:
         self.items.append(payload.payload)
@@ -21,15 +25,19 @@ class DummyProducer(StreamProducer):
     def poll(self, timeout: float) -> None:
         pass
 
+    def flush(self, timeout: float) -> int:
+        pass
+
 
 class TestSnapshotControl:
     def test_init(self) -> None:
         uuid = uuid1()
-        producer = DummyProducer()
+        backend = DummyProducerBackend()
+        producer = StreamProducer(backend)
         control = SnapshotControl(producer, {})
         control.init_snapshot(snapshot_id=uuid, tables=["my_table"], product="snuba")
 
-        reloaded = json.loads(producer.items[0])
+        reloaded = json.loads(backend.items[0])
         assert reloaded == {
             "snapshot-id": str(uuid),
             "product": "snuba",
